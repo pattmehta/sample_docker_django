2c5545080fd61fb88986f36644584ee9082af949
Prateek Mehta <pattmehta.official@gmail.com>
2023-12-23 20:28:45 -0800

    adds script for mock data

---
+++ scripts/host/build_image.sh
@@ -0,0 +1,18 @@
+# note: run from root folder as `bash ./scripts/...`
+IMGNAME=$1
+P3PATH=`which python3`
+PYTHONCMD=""
+
+if [ -z "${IMGNAME}" ]; then
+    echo "please enter image-name as first param"
+    exit 1
+fi
+
+if [ -z "${P3PATH}" ]; then
+    PYTHONCMD="python"
+else
+    PYTHONCMD="python3"
+fi
+
+${PYTHONCMD} ./scripts/host/build_env.py
+docker build -t ${IMGNAME} .
