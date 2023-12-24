b76a4294e1feb48da463c36bbefaab0eefb1b6e8
Prateek Mehta <pattmehta.official@gmail.com>
2023-12-23 17:43:15 -0800

    adds packages for media-images, adds settings for media url/root, corr changes in model and urls

--- temphapis/models.py
+++ temphapis/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class History(models.Model):
     item = models.CharField(max_length=100)
 
 class UserHistory(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     item = models.CharField(max_length=100)
+
+class Image(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    thumbnail = models.SlugField(null=True, blank=True, max_length=100)
--- temphapis/urls.py
+++ temphapis/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index),
@@ -7,5 +9,9 @@ urlpatterns = [
     path("token/", views.token),
     path("history/", views.history),
     path("user_history/", views.user_history),
-    path("reset_lockout/", views.reset_lockout, name='reset-lockout')
-]
+    path("reset_lockout/", views.reset_lockout, name='reset-lockout'),
+    path("upload_image/", views.upload_image)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# on localhost uploaded image is at:
+# http://127.0.0.1:8095/api/media/images/user_third_img.png
