9a6af432c8e62f78193b738924c7f8d386153de9
Prateek Mehta <pattmehta.official@gmail.com>
2024-01-01 23:22:42 -0800

    updates env path for keys

--- envconfig.py
+++ envconfig.py
@@ -6,10 +6,14 @@ class EnvConfig:
     def __init__(self):
         self._config = {
             **dotenv_values("env/.env"),  # load shared development variables
-            **dotenv_values("env/.env.secret"),  # load sensitive variables
+            **EnvConfig.get_secret_pairs("env/.env.secret"),  # load sensitive variables
             **os.environ,  # override loaded values with environment variables
         }
         self._keys = list(self._config.keys())
+
+    @staticmethod
+    def get_secret_pairs(filename):
+        return dotenv_values(filename) if os.path.exists(filename) else {'DBUSER':'app_user','DBPASS':'app_password','SECRET_KEY':'longstring','JWT_SIGNING_KEY':'longstring'}
 
     def keys(self,count = 0):
         self._keys = list(self._config.keys())
